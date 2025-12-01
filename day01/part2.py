17ce24452918c63728e5a2f5e3bc4e5e548d8669
Samarth Pai <samarthpai9870@gmail.com>
2025-12-01 11:18:25 +0530

    Added .gitignore

--- day01/part2.py
+++ day01/part2.py
@@ -6,7 +6,7 @@ password = 0
 for line in lines:
     dir = line[0]
     num = int(line[1:])
-    if dir == 'R':
+    if dir == 'R':  
         if num + dial >= 100:
             password += (num + dial) // 100
         dial = (dial + num) % 100
