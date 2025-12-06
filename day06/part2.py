c7e9d844a951b7a487ee7f5d2e09bf31d7eb45dd
Samarth Pai <samarthpai9870@gmail.com>
2025-12-06 11:12:06 +0530

    Replaced else: if with elif for part 2

--- day06/part2.py
+++ day06/part2.py
@@ -11,9 +11,8 @@ for i in range(len(lines[0])):
     for j in range(len(lines)):
         if lines[j][i].isdigit():
             num += lines[j][i]
-        else:
-            if lines[j][i] in "*+":
-                op = lines[j][i]
+        elif lines[j][i] in "*+":
+            op = lines[j][i]
     if num == "":
         if op == "*":
             ans += reduce(mul, nums)
