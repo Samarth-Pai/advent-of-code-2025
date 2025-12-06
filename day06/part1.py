82ca7b05a23b6ca2a343b3e19fd9eef5bad96c82
Samarth Pai <samarthpai9870@gmail.com>
2025-12-06 11:10:39 +0530

    Removed unecessary print statement in part 1

--- day06/part1.py
+++ day06/part1.py
@@ -7,7 +7,6 @@ nums = []
 for line in lines[:-1]:
     nums.append(list(map(int, line.split())))
 ans = 0
-print(len(lines))
 for en, op in enumerate(lines[-1].split()):
     col = [nums[i][en] for i in range(len(lines)-1)]
     if op == "*":
